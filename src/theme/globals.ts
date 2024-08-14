938c7496329648afaaaa0413d2f5e13341066017
Pedro Paulo <pedropaulo.developer@gmail.com>
2024-08-14 15:52:02 -0300

    feat(packages): Adicionado lib do Material MUI e zustand
    - Adicionado configuração de estilos globais
    - Adicionado links no head da página relacionados ao Material MUI

---
+++ src/theme/globals.ts
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  import '@fontsource/roboto/300.css';
+  import '@fontsource/roboto/400.css';
+  import '@fontsource/roboto/500.css';
+  import '@fontsource/roboto/700.css';
+
+`;
+
+export default GlobalStyles;
